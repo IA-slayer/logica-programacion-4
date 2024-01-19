ddd085be0813ea4b8517f50c0bdce00a3eb9288a
richard mejias <richard987@hotmail.cl>
2024-01-18 23:54:45 -0300

    commit trabajo terminado

--- script.js
+++ script.js
@@ -6,26 +6,41 @@
 //    return celsius + 273.15;
 //}
 
-function factorial(num) {
-    if (num === 0 || num === 1) {
-      return 1;
-    } else {
-      return num * factorial(num - 1);
+//function factorial(num) {
+//    if (num === 0 || num === 1) {
+//      return 1;
+//    } else {
+//      return num * factorial(num - 1);
+//    }
+//  }
+
+function fibonacci(n) {
+  if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    const fibSeries = [0, 1];
+    while (fibSeries.length < n) {
+      fibSeries.push(fibSeries[fibSeries.length - 1] + fibSeries[fibSeries.length - 2]);
     }
+    return fibSeries;
   }
+}
 
 function conversor() {
-    let numfact = parseInt(document.getElementById("factInput").value);
+    let numFibo = parseInt(document.getElementById("fiboInput").value);
 
-    if (!isNaN(numfact)) {
-        let fact = factorial(numfact).toFixed(2);
+    if (!isNaN(numFibo) && numFibo > 0) {
+        let serieFibo = fibonacci(numFibo).join(`, `);
         //let tempKelvin = celsiusKelvin(tempCelsius).toFixed(2);
 
         let resultadosDiv = document.getElementById("resultados");
-        resultadosDiv.innerHTML = "<p>" + " El factorial de " + numfact + " es " + fact + "</p>";
+        resultadosDiv.innerHTML = "<p>" + " Los primeros " + numFibo + " de la serie de Fibonacci es : " + serieFibo + "</p>";
         //resultadosDiv.innerHTML += "<p>" + tempCelsius + " grados Celsius son " + tempKelvin + " Kelvin.</p>";
+        
 
     } else {
-        alert("Error, por favor ingrese un número ya que el conversor solo acepta números como entradas.");
+        alert("Error, por favor ingrese un número natural ya que el programa solo acepta números naturales como entradas.");
     }
 }
